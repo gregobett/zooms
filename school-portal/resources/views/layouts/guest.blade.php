284a6519639f2cd2c6c248cb440b206628c9431e
giovanni kuan <giokuan@gmail.com>
2023-06-15 09:15:50 +0800

    fixed the flashing effects bug when transition to other page

--- school-portal/resources/views/layouts/guest.blade.php
+++ school-portal/resources/views/layouts/guest.blade.php
@@ -14,12 +14,16 @@
         href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet" />
 
-
+ 
     <!-- Styles -->
     <style>
         [x-cloak] {
             display: none;
         }
+
+        .hide-content {
+        display: none;
+}
     </style>
     
     <!-- Scripts -->
@@ -27,8 +31,8 @@
 </head>
 
 <body >
-    
-    <div class="font-sans antialiased h-screen w-full  bg-no-repeat bg-cover bg-center" x-data="mainState" :class="{dark: isDarkMode}" x-cloak   style="background-image: url({{ url('images/header.webp') }}) " >
+<div id="page-content" class="hide-content">
+    <div class="font-sans antialiased h-screen w-full  bg-no-repeat  bg-cover bg-center" x-data="mainState" :class="{dark: isDarkMode}" x-cloak   style="background-image: url({{ url('images/header.webp') }}); " >
         
     
         <div class="flex flex-col min-h-screen text-gray-900 pt-10 lg:pt-12 xl:pt-0 opacity-75 dark:text-gray-200 2xl:pt-16" >
@@ -40,7 +44,15 @@
 
    
     </div>
-
+ 
+</div>
+<script>
+    const backgroundImage = new Image();
+    backgroundImage.src = "{{ url('images/header.webp') }}";
+    backgroundImage.addEventListener('load', function() {
+        document.getElementById('page-content').classList.remove('hide-content');
+    });
+</script>
 
 </body>
 
